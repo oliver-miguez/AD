69d1c69c336a02a614a4859517728a3871717b57
XogosAquiDEV <olimial03@gmail.com>
2025-09-10 19:22:53 +0200

    Ejercicio 7 Apartado 1

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,9 @@ public class Main{
         //eFicheiro("/home/dam/Documentos/AD");
         //creaDirectorio("/home/dam/Documentos/AD/P1-1");
         //creaFicheiro("C:\\Users\\olimi\\Desktop\\2ºDam\\AD","ArchivoCreadoPorCódigo");
-        modoAcceso("C:\\Users\\olimi\\Desktop\\2ºDam\\AD","SePuedeLeerOEscribir");
+        //modoAcceso("C:\\Users\\olimi\\Desktop\\2ºDam\\AD","SePuedeLeerOEscribir");
+        //calculaLonxitude("C:\\Users\\olimi\\Desktop\\2ºDam\\AD","OtroArchivo");
+         mLectura("C:\\Users\\olimi\\Desktop\\2ºDam\\AD","ArchivoCreadoPorCódigo");
     }//en static
 
 
@@ -61,4 +63,26 @@ public class Main{
             }
         }
     }
+    public static void calculaLonxitude(String dirName, String fileName){
+        long longitudBytes = 0;
+        File directorio = new File(dirName,fileName);
+        longitudBytes =  directorio.length();
+        System.out.println("Total de bytes:"+ longitudBytes);
+    }
+
+    //7) método mLectura (dirName, fileName)
+    //Dada a ruta absoluta e nome dun arquivo, este método debe facer o arquivo de so
+    //lectura.
+
+    public static void mLectura(String dirName, String fileName){
+        File archivo = new File(dirName,fileName);
+        boolean correcto = archivo.setReadOnly();
+        if(correcto){
+            System.out.println("El directorio se puede leer");
+        }else{
+            System.out.println("Error, creo que esto no va bien ");
+        }
+    }
+
+
 }//end class
