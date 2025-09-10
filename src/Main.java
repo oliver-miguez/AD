6f1dd76bace47336906047f8b01fc00b7ce5ce25
XogosAquiDEV <olimial03@gmail.com>
2025-09-10 17:43:20 +0200

    Eje 4 Parte 1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import java.io.File;
+import java.io.IOException;
+
 public class Main{
-    public static void main(String[] args) {
-        eDirectorio("/home/dam/Documentos/AD");
-        eFicheiro("/home/dam/Documentos/AD");
-        creaDirectorio("/home/dam/Documentos/AD/P1-1");
+    public static void main(String[] args) throws IOException {
+        //eDirectorio("/home/dam/Documentos/AD");
+        //eFicheiro("/home/dam/Documentos/AD");
+        //creaDirectorio("/home/dam/Documentos/AD/P1-1");
+        creaFicheiro("C:\\Users\\olimi\\Desktop\\2ºDam\\AD","ArchivoCreadoPorCódigo");
     }//en static
 
 
@@ -33,10 +36,13 @@ public class Main{
             System.out.println("No se ha creado");
         }
     }
-
-
-    public static void creaFicheiro(String dirName, String fileName){
-        File archivo = new File(dirName);
+    public static void creaFicheiro(String dirName, String fileName) throws IOException {
+        File archivo = new File(dirName,fileName);
+        if(archivo.createNewFile()){
+            System.out.println("Archivo creado correctamente en el directorio");
+        }else{
+            System.out.println("Problemas con la creación del archivo");
+        }
 
     }
 }//end class
