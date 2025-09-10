9ba1fec756c12f78770d43329f4fa10028233cae
XogosAquiDEV <olimial03@gmail.com>
2025-09-10 19:36:53 +0200

    Haciendo eje 8

--- src/Main.java
+++ src/Main.java
@@ -69,11 +69,6 @@ public class Main{
         longitudBytes =  directorio.length();
         System.out.println("Total de bytes:"+ longitudBytes);
     }
-
-    //7) método mLectura (dirName, fileName)
-    //Dada a ruta absoluta e nome dun arquivo, este método debe facer o arquivo de so
-    //lectura.
-
     public static void mLectura(String dirName, String fileName){
         File archivo = new File(dirName,fileName);
         boolean correcto = archivo.setReadOnly();
@@ -84,5 +79,19 @@ public class Main{
         }
     }
 
+    /**
+    //8) método mEscritura (dirName, fileName)
+    //Dada a ruta e nome dun arquivo, este método debe facer posible escribir no arquivo.
+    public static void mEscritura(String dirName, String fileName){
+        File archivo = new File(dirName,fileName);
+        boolean correcto = archivo;
+        if(correcto){
+            System.out.println("El directorio se puede leer");
+        }else{
+            System.out.println("Error, creo que esto no va bien ");
+        }
+    }
+*/
+}
 
 }//end class
