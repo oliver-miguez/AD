7c7b5367525f5ee94df6050948802fade1795ffb
oliver <omiguezalonso@gmail.com>
2025-09-12 13:40:13 +0200

    Eje 9,10 Apartado 1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,88 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        //eDirectorio("/home/dam/Documentos/AD");
-        //eFicheiro("/home/dam/Documentos/AD");
-        //creaDirectorio("/home/dam/Documentos/AD/P1-1");
-        //creaFicheiro("C:\\Users\\olimi\\Desktop\\2ºDam\\AD","ArchivoCreadoPorCódigo");
-        //modoAcceso("C:\\Users\\olimi\\Desktop\\2ºDam\\AD","SePuedeLeerOEscribir");
-        //calculaLonxitude("C:\\Users\\olimi\\Desktop\\2ºDam\\AD","OtroArchivo");
-         //mLectura("C:\\Users\\olimi\\Desktop\\2ºDam\\AD","ArchivoCreadoPorCódigo");
-         mEscritura("/home/dam/Documentos/AD/P1-1 Ficheiros/src","mEscritura");
-    }//en static
+        Scanner teclado = new Scanner(System.in);
+        int opcion;
+
+        // Aquí defines las rutas fijas para tus pruebas
+        String directorioBase = "/home/dam/Documentos/AD/P1-1 Ficheiros/src";
+        String directorioBorrar = "/home/dam/Documentos/AD/P1-1 Ficheiros/src/NuevoDir";
+        String archivoBase = "ArchivoCreadoPorCódigo";
+
+        do {
+            System.out.println("--- MENÚ DE OPERACIONES DE ARCHIVOS ---");
+            System.out.println("Ruta de prueba: " + directorioBase);
+            System.out.println("Archivo de prueba: " + archivoBase);
+            System.out.println("------------------------------------");
+            System.out.println("1.  Comprobar si es un directorio");
+            System.out.println("2.  Comprobar si es un archivo");
+            System.out.println("3.  Crear directorio");
+            System.out.println("4.  Crear archivo");
+            System.out.println("5.  Mostrar modo de acceso (lectura/escritura)");
+            System.out.println("6.  Calcular tamaño en bytes");
+            System.out.println("7.  Establecer modo de solo lectura");
+            System.out.println("8.  Establecer modo de escritura");
+            System.out.println("9.  Borrar archivo");
+            System.out.println("10. Borrar directorio");
+            System.out.println("0.  Salir");
+            System.out.print("Elige una opción: ");
+            opcion = teclado.nextInt();
+            teclado.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    if (eDirectorio(directorioBase)) {
+                        System.out.println("¡Es un directorio!");
+                    } else {
+                        System.out.println("No es un directorio.");
+                    }
+                    break;
+                case 2:
+                    File archivoCompleto = new File(directorioBase, archivoBase);
+                    if (eFicheiro(archivoCompleto.getAbsolutePath())) {
+                        System.out.println("¡Es un archivo!");
+                    } else {
+                        System.out.println("No es un archivo.");
+                    }
+                    break;
+                case 3:
+                    creaDirectorio(directorioBase + "/NuevoDir");
+                    break;
+                case 4:
+                    creaFicheiro(directorioBase, archivoBase);
+                    break;
+                case 5:
+                    modoAcceso(directorioBase, archivoBase);
+                    break;
+                case 6:
+                    calculaLonxitude(directorioBase, archivoBase);
+                    break;
+                case 7:
+                    mLectura(directorioBase, archivoBase);
+                    break;
+                case 8:
+                    mEscritura(directorioBase, archivoBase);
+                    break;
+                case 9:
+                    borraFicheiro(directorioBase, archivoBase);
+                    break;
+                case 10:
+                    borrarDirectorio(directorioBorrar);
+                    break;
+                case 0:
+                    System.out.println("cerrando");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intenta de nuevo.");
+            }
+            System.out.println(); // Línea en blanco para mejor formato
+        } while (opcion != 0);
+
+        teclado.close();
+    }
 
 
     public static boolean eDirectorio(String cadea){
@@ -62,6 +133,17 @@ public class Main {
             }else{
                 System.out.println("No se puede escribir");
             }
+        }else{
+            if(directorio.canRead()){
+                System.out.println("Se puede leer");
+            }else{
+                System.out.println("No puede leer");
+            }
+            if(directorio.canWrite()){
+                System.out.println("Se puede escribir");
+            }else{
+                System.out.println("No se puede escribir");
+            }
         }
     }
     public static void calculaLonxitude(String dirName, String fileName){
@@ -79,13 +161,32 @@ public class Main {
             System.out.println("Error, creo que esto no va bien ");
         }
     }
-    public static void mEscritura(String dirName, String fileName) throws IOException {
+    public static boolean mEscritura(String dirName, String fileName) throws IOException {
         File archivo = new File(dirName,fileName);
-        if(archivo.createNewFile()){
-            boolean permiso = archivo.setWritable(true); //modificar con true o false dependiendo si queremos que se pueda o no escribir
-        }
+        boolean permiso = archivo.setWritable(true);
+        return permiso;
 
     }
-
+    public static void borraFicheiro(String dirName, String fileName) throws IOException {
+        File archivo = new File(dirName,fileName);{
+            System.out.println("Nombre del archivo: " + fileName);
+            boolean borrar = archivo.delete();
+            if(borrar){
+                System.out.println("borrado");
+            }else{
+                System.out.println("No se ha borrado");
+            }
+        }
+    }
+    public static void borrarDirectorio(String dirName){
+        File archivo = new File(dirName);
+        System.out.println("Nombre directorio:"+dirName);
+        boolean borrarDir = archivo.delete();
+        if (borrarDir){
+            System.out.println("Borrado");
+        }else{
+            System.out.println("No borrado");
+        }
+    }
 
 }//end class
